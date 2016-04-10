a93bd70445e1f9e9014aaa140dec3c01ae7b5429
marcelomedina87 <marcelomedina_87@hotmail.com>
2016-04-10 17:17:43 -0100

    Delete funciones.h

--- funciones.h
+++
@@ -1,6 +0,0 @@
-float pedirNumero(); // Funcion q pide un numero y lo retorna.
-float suma(float a, float b); // Funcion sumar dos numeros.
-float resta(float a, float b); // Funcion restar dos numeros.
-float multiplicar(float a, float b); // Funcion multiplicar dos numeros.
-float dividir(float a, float b); // Funcion dividir dos numeros.
-long factorial(float n); // Funcion factorial de un numero.
